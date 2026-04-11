1886251f7ec781a337b38a81a4ad3447a268707b
ahadzada13 <13739@holbertonstudents.com>
2026-04-11 14:36:50 +0000

    Task 0: My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+It is part of a series of exercises to learn about classes and objects.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square.
+    This class does not have any attributes or methods yet.
+    """
+    pass
