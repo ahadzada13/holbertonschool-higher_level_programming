a54d129600a30f7bd7b30088eab3211516cf3a68
Mehmana Ahadova <13739@holbertonstudents.com>
2026-04-16 21:04:32 +0000

    Add task 10: top score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table (database hbtn_0c_0)
+-- Results display both score and name, ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
