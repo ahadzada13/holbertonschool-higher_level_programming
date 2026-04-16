b054eee74c0a10cde68585f7bb096c45891b850a
Mehmana Ahadova <13739@holbertonstudents.com>
2026-04-16 22:04:26 +0000

    Add task 6: POST email with requests

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email as a parameter
+using the requests package and displays the response body.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    r = requests.post(url, data=payload)
+    print(r.text)
