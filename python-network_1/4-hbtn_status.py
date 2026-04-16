6a865f3dce37248bb46860737d0ace64914f323d
Mehmana Ahadova <13739@holbertonstudents.com>
2026-04-16 22:01:36 +0000

    TASK fixed

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,8 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    
+    r = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
